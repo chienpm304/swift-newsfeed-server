daaf3794e7a7766735a2423150fda921b60109e7
Chien Pham <minhchien.pham@shopee.com>
2024-05-02 18:49:02 +0700

    Updated sites, limited posts per site to 50, and filter out posts prior to 2023

--- data/process.py
+++ data/process.py
@@ -2,13 +2,14 @@ from utils import *
 
 # Constants
 slugs = ["development", "updates", "packages", "design", "marketing", "companies", "podcasts", "newsletters", "youtube", "twitch"]
+large_slugs = ["development"]
 unsupported_site_urls = load_txt_lines("unsupported_site_urls.txt")
 IS_DEBUG = True
 LIMIT_NUMBER_OF_POST_PER_SITE = 50
-LIMIT_NUMBER_OF_POST_PER_SLUG = 1000
+ARCHIVED_TIMESTAMP = 1672531200
 
 def rebuild_sites():
-    data = load_json("./categories.json")
+    data = load_json("./blogs.json")[0] # English only now
 
     all_sites = []
     for category in data.get("categories", []):
@@ -17,6 +18,8 @@ def rebuild_sites():
         
         sites = category.get("sites", [])
         for site in sites:
+            if site["site_url"] in unsupported_site_urls:
+                continue
             site_dict = {
                 "title": site.get("title", ""),
                 "site_url": site.get("site_url", ""),
@@ -60,29 +63,35 @@ def parse_feed(site):
     return posts
 
 def generate_posts_by_sites(sites):
-    for site in sites:
-        try:
-            output_file_path = "./posts/by_site/" + simplifized_url(site["site_url"]) + ".json"
-            print(f"processing {output_file_path}")
+    with open('log_error_site.txt', 'w') as log_file:
+        for site in sites:
+            try:
+                output_file_path = "./posts/by_site/" + simplifized_url(site["site_url"]) + ".json"
+                print(f"processing {output_file_path}")
 
-            if IS_DEBUG and os.path.isfile(output_file_path):
-                print(f"[debug] Skipped: {output_file_path}")
-                posts = load_json(output_file_path)["posts"]
-            else:
-                posts = parse_feed(site)
-            
-            if not posts:
-                raise Exception("empty posts")
-            else:
-                posts.sort(reverse=True, key=lambda t: t["updated"])
-                posts_result = {
-                    "updated": current_timestamp(),
-                    "posts": posts
-                }
-                save_json(posts_result, output_file_path)
+                if IS_DEBUG and os.path.isfile(output_file_path):
+                    print(f"[debug] Skipped: {output_file_path}")
+                    posts = load_json(output_file_path)["posts"]
+                else:
+                    posts = parse_feed(site)
+                
+                if not posts:
+                    raise Exception("empty posts")
+                else:
+                    posts.sort(reverse=True, key=lambda t: t["updated"])
+                    posts_result = {
+                        "updated": current_timestamp(),
+                        "posts": posts
+                    }
+                    save_json(posts_result, output_file_path)
 
-        except Exception as e:
-            print(f"❌ Error parsing, {site['site_url']}, error: {str(e)}")
+                    # log timestamp
+                    # timestamp = posts[0]['updated']
+                    # log_file.write(f"{site['site_url']}, {len(posts)}, {timestamp}\n")
+
+            except Exception as e:
+                print(f"❌ Error parsing, {site['site_url']}, error: {str(e)}")
+                log_file.write(f"{site['site_url']}\n")
 
 def generate_posts_by_slugs(sites):
     for slug in slugs:
@@ -93,8 +102,8 @@ def generate_posts_by_slugs(sites):
                 continue
 
             site_posts = load_json(file_path)["posts"]
-            # site_posts = list(filter(lambda t: (t["updated"] > 1525062600), site_posts))
-            # site_posts = site_posts[slice(LIMIT_NUMBER_OF_POST_PER_SITE)]
+            site_posts = list(filter(lambda t: (t["updated"] > ARCHIVED_TIMESTAMP), site_posts))
+            site_posts = site_posts[slice(LIMIT_NUMBER_OF_POST_PER_SITE)]
             
             for post in site_posts:
                 slug_posts.append(post)
